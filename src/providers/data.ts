974e6cdfdfcb0e97b8fc37e6e7eb767358027584
Sayandeep <sdroy001@gmail.com>
2026-02-01 00:27:50 +0530

    Updated package dependencies and improved constants for user roles and departments. Refactored data provider to utilize backend API and handle filtering for subjects. Adjusted subject list component to use new department options.

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,21 +1,49 @@
-import { BaseRecord, DataProvider, GetListParams, GetListResponse } from "@refinedev/core";
-import { mockSubjects } from "../constants/mock-data";
+import { BACKEND_BASE_URL } from "@/constants";
+import { ListResponse } from "@/types";
+import { createDataProvider, CreateDataProviderOptions } from "@refinedev/rest";
 
-export const dataProvider: DataProvider = {
-  getList: async <TData extends BaseRecord = BaseRecord> ({resource } : GetListParams): Promise<GetListResponse<TData>> => {
-    if(resource !== 'subjects') return { data: [] as TData[], total: 0 };
-    
-    return {
-      data: mockSubjects as unknown as TData[],
-      total: mockSubjects.length,
-    }
-  },
-
-  getOne: async () => {throw new Error("Method not implemented.")},
-  create: async () => {throw new Error("Method not implemented.")},
-  update: async () => {throw new Error("Method not implemented.")},
-  deleteOne: async () => {throw new Error("Method not implemented.")},
-
-  getApiUrl: () => '',
-
+if(!BACKEND_BASE_URL) {
+  throw new Error('BACKEND_BASE_URL is not set');
 }
+
+const options: CreateDataProviderOptions = {
+  getList: {
+    getEndpoint: ({ resource }) => resource,
+    buildQueryParams: async ({ filters, resource, pagination }) => {
+      const page = pagination?.currentPage ?? 1;
+      const pageSize = pagination?.pageSize ?? 10;
+      const params: Record<string, string|number> = {page, limit: pageSize};
+
+      filters?.forEach((filter) => {
+        const field = 'field' in filter ? filter.field : '';
+
+        const value = String(filter.value);
+
+        if(resource === 'subjects') {
+          if(field === 'department') {
+            params.department = value;
+          }
+          if(field === 'name' || field === 'code') {
+            params.search = value;
+          }
+        }
+
+      })
+
+      return params;
+    },
+
+    mapResponse: async (response) => {
+      const payload: ListResponse = await response.clone().json();
+      return payload.data ?? [];
+    },
+    getTotalCount: async (response) => {
+      const payload: ListResponse = await response.clone().json();
+      return payload.pagination?.total ?? payload.data?.length ?? 0;
+    }
+  }
+}
+
+const {dataProvider} = createDataProvider(BACKEND_BASE_URL, options);
+
+export {dataProvider};
